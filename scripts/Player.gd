8db72fa703f4b68a4003fabb9d3946ff6318a9f0
JohnyBuk <jenahruby@gmail.com>
2020-08-10 22:14:23 +0200

    proper idles

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,10 +1,44 @@
 extends Area2D
 
-export var speed = 400  # How fast the player will move (pixels/sec).
-var screen_size  # Size of the game window.
+export var speed = 400 
+var screen_size  
+var last_direction = Vector2(0, 1)
+
 
 func _ready():
 	screen_size = get_viewport_rect().size
+
+
+func choose_animation(velocity: Vector2):
+	velocity = velocity.normalized()
+	var a = sqrt(2)/2
+
+	if velocity.y >= a:
+		return ["down", false]
+	elif velocity.y <= -a:
+		return ["up", false]
+	elif velocity.x <= -a:
+		return ["left", false]
+	elif velocity.x >= a:
+		return ["left", true]
+	return ["down", false]
+
+
+func animates_player(velocity: Vector2):
+	var animation_name
+	var result
+	
+	if velocity.length_squared() > 0:
+		last_direction = velocity
+		result = choose_animation(last_direction)
+		animation_name = result[0] + "_walk"
+		
+	else:
+		result = choose_animation(last_direction)
+		animation_name = result[0] + "_idle"
+
+	$AnimatedSprite.play(animation_name)
+	$AnimatedSprite.flip_h = result[1]
 
 
 func _process(delta):
@@ -26,14 +60,5 @@ func _process(delta):
 	position += velocity * delta
 	position.x = clamp(position.x, 0, screen_size.x)
 	position.y = clamp(position.y, 0, screen_size.y)
-	
-	if velocity.x != 0:
-		$AnimatedSprite.animation = "left"
-		$AnimatedSprite.flip_h = velocity.x > 0
-	elif velocity.y > 0:
-		$AnimatedSprite.animation = "down"
-	elif velocity.y < 0:
-		$AnimatedSprite.animation = "up"
-	else:
-		$AnimatedSprite.animation = "idle"
-		
+
+	animates_player(velocity)
