b82ad428c62490adb2d5a051262d57db16cfc96c
JohnyBuk <jenahruby@gmail.com>
2020-08-13 22:54:07 +0200

    skeleton proper z index

--- scripts/Skeleton.gd
+++ scripts/Skeleton.gd
@@ -3,5 +3,7 @@ extends KinematicBody2D
 export var speed = 5 
 
 func _physics_process(delta):
-	var velocity = (get_node("../Player").position - position).normalized()
+	var player_direction = get_node("../Player").position - position
+	var velocity = player_direction.normalized()
 	move_and_collide(velocity * speed)
+	z_index = -player_direction.y
