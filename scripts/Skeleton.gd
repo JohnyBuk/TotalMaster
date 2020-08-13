b00d46ca50693c6399f605576e94920629c1c7ed
JohnyBuk <jenahruby@gmail.com>
2020-08-14 00:08:37 +0200

    skeletons start with different animation frame

--- scripts/Skeleton.gd
+++ scripts/Skeleton.gd
@@ -2,11 +2,17 @@ extends Node2D
 
 var speed = 100
 
+
+func _ready():
+	var rng = RandomNumberGenerator.new()
+	rng.randomize()
+	$AnimatedSprite.frame = rng.randi_range(0, 5)
+
+
 func move(velocity: Vector2):
 	var collision = $Head.move_and_collide(velocity)
 	$AnimatedSprite.position = $Head.position 
 	$Body.position = $Head.position 
-	print(speed)
 
 func _physics_process(delta):
 	var player_direction = get_node("../Player/Body").global_position -  $Head.global_position
